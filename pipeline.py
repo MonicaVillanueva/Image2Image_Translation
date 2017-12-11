2920db5a4ec0dc7b5eb478fbf493ff88a43ca9c4
Serlopal <oro_sergio@hotmail.com>
2017-12-11 13:58:44 +0100

    initial pipeline schema

---
+++ pipeline.py
@@ -0,0 +1,15 @@
+
+
+class Pipeline()
+    def __init__(self, config):
+        self.param1 = config.parameter1
+        self.param2 = config.parameter2
+
+    def init_model:
+        # Initilizes the generator and discriminator
+        self.Gen = Generator()
+        self.Dis = Discriminator()
+
+    def train:
+
+    def test:
