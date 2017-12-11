ae8b03fe99f1e14b3a78ce15a13b65bea4142566
Serlopal <oro_sergio@hotmail.com>
2017-12-11 12:59:01 +0100

    basic main schema

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 
+import argparse
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    # Model parameters
+    parser.add_argument('--parameter1', type=int, default=0)
+    parser.add_argument('--parameter2', type=double, default=0)
+
+    config = parser.parse_args()
+    print(config)
+    main(config)
 
 
