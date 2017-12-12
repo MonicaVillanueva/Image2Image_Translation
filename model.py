94c1274f5424e12b871ea8511368dfc390fc3528
HectorAnadon <hectoranadonleon@gmail.com>
2017-12-12 15:24:07 +0100

    Generator architecture

--- model.py
+++ model.py
@@ -34,37 +34,65 @@ class ResidualBlock():
 
 class Generator():
 
-	def __init__(self, convDim=64, numClass=5):
+	def __init__(self, imgDim=(64,64), numClass=5):
 		# Weights initialised
+		self.imgDim = imgDim;
 
-		downSampling1_W = weight_variable([7, 7, 3 + numClass, 64])
-		downSampling1_b = bias_variable([64])
+		self.downSampling1_W = weight_variable([7, 7, 3 + numClass, 64])
+		self.downSampling1_b = bias_variable([64])
 
-		downSampling2_W = weight_variable([4, 4, 64, 128])
-		downSampling2_b = bias_variable([128])
+		self.downSampling2_W = weight_variable([4, 4, 64, 128])
+		self.downSampling2_b = bias_variable([128])
 
-		downSampling3_W = weight_variable([4, 4, 128, 256])
-		downSampling3_b = bias_variable([256])
+		self.downSampling3_W = weight_variable([4, 4, 128, 256])
+		self.downSampling3_b = bias_variable([256])
 
-		residualBlock1 = ResidualBlock();
-		residualBlock2 = ResidualBlock();
-		residualBlock3 = ResidualBlock();
-		residualBlock4 = ResidualBlock();
-		residualBlock5 = ResidualBlock();
-		residualBlock6 = ResidualBlock();
+		self.residualBlock1 = ResidualBlock();
+		self.residualBlock2 = ResidualBlock();
+		self.residualBlock3 = ResidualBlock();
+		self.residualBlock4 = ResidualBlock();
+		self.residualBlock5 = ResidualBlock();
+		self.residualBlock6 = ResidualBlock();
 
-		upSampling1_W = weight_variable([4, 4, 256, 128])
-		upSampling1_b = bias_variable([128])
+		self.upSampling1_W = weight_variable([4, 4, 256, 128])
+		self.upSampling1_b = bias_variable([128])
 
-		upSampling2_W = weight_variable([4, 4, 128, 64])
-		upSampling2_b = bias_variable([64])
+		self.upSampling2_W = weight_variable([4, 4, 128, 64])
+		self.upSampling2_b = bias_variable([64])
 
-		upSampling3_W = weight_variable([7, 7, 64, 3])
-		upSampling3_b = bias_variable([3])
+		self.upSampling3_W = weight_variable([7, 7, 64, 3])
+		self.upSampling3_b = bias_variable([3])
 
 
 	def forward(self, x, c):
-		# TODO: concatenate c to x
+		# x has to contain already the labels concatenated
+
+		X = tf.placeholder(tf.float32, shape=[None, self.imgDim[0], self.imgDim[1], len(c)])
+
+		Y_downSampling1 = tf.nn.relu(tf.nn.conv2d(X, self.downSampling1_W, strides=[1, 1, 1, 1], padding='SAME') + self.downSampling1_b)
+		Y_downSampling1_norm = tf.contrib.layers.batch_norm(Y_downSampling1)
+
+		Y_downSampling2 = tf.nn.relu(tf.nn.conv2d(Y_downSampling1_norm, self.downSampling2_W, strides=[1, 2, 2, 1], padding='SAME') + self.downSampling2_b)
+		Y_downSampling2_norm = tf.contrib.layers.batch_norm(Y_downSampling2)
+
+		Y_downSampling3 = tf.nn.relu(tf.nn.conv2d(Y_downSampling2_norm, self.downSampling3_W, strides=[1, 2, 2, 1], padding='SAME') + self.downSampling3_b)
+		Y_downSampling3_norm = tf.contrib.layers.batch_norm(Y_downSampling3)
+
+		Y_residual1 = self.residualBlock1.forward(Y_downSampling3_norm)
+		Y_residual2 = self.residualBlock2.forward(Y_residual1)
+		Y_residual3 = self.residualBlock3.forward(Y_residual2)
+		Y_residual4 = self.residualBlock4.forward(Y_residual3)
+		Y_residual5 = self.residualBlock5.forward(Y_residual4)
+		Y_residual6 = self.residualBlock6.forward(Y_residual5)
+
+		Y_upSampling1 = tf.nn.relu(tf.nn.conv2d_transpose(Y_residual6, self.upSampling1_W, strides=[1, 2, 2, 1], padding='SAME') + self.upSampling1_b)
+		Y_upSampling1_norm = tf.contrib.layers.batch_norm(Y_upSampling1)
+
+		Y_upSampling2 = tf.nn.relu(tf.nn.conv2d_transpose(Y_upSampling1_norm, self.upSampling2_W, strides=[1, 2, 2, 1], padding='SAME') + self.upSampling2_b)
+		Y_upSampling2_norm = tf.contrib.layers.batch_norm(Y_upSampling2)
+
+		Y_upSampling3 = tf.nn.tanh(tf.nn.conv2d(Y_upSampling2_norm, self.upSampling3_W, strides=[1, 1, 1, 1], padding='SAME') + self.upSampling3_b)
+
 
 
 class Discriminator():
